4f3c4ee16991b52fcd1f44569b0456501f9e6b2a
yunushircin <yunushircin@gmail.com>
2023-03-13 13:19:15 +0300

    notification update

--- base/views.py
+++ base/views.py
@@ -80,22 +80,24 @@ def home(request):
 
 def userPage(request):
     user = request.user
-    problems = user.problem_set.all()
+    # problems = user.problem_set.all()
+    problems = Problem.objects.filter(user=request.user)
 
+    unread_notification = Notification.objects.filter(user=request.user, read=False).count()
     total_problems = problems.count()
     open = problems.filter(status='Açık').count()
     close = problems.filter(status='Kapalı').count()
 
-
     problem_filter = ProblemFilter(request.GET, queryset=problems)
     problems = problem_filter.qs
 
-    context = {'problems':problems,'user':user, 'total_problems':total_problems,'open':open,'close':close,'problem_filter':problem_filter}
+    context = {'problems':problems,'user':user, 'total_problems':total_problems,'open':open,'close':close,'problem_filter':problem_filter, 'unread_notification':unread_notification}
     return render(request, 'base/user.html', context)
 
 
 def createProblem(request):
 
+    unread_notification = Notification.objects.filter(user=request.user, read=False).count()
     form = ProblemForm(request.POST if request.method == 'POST' else None, initial={'add_by':request.user})
     
     if request.method == 'POST':
@@ -104,18 +106,23 @@ def createProblem(request):
             problem.save()
 
             message = f'{problem.add_by} size yeni bir görev oluşturdu'
-            Notification.objects.create(user=User.objects.get(username=problem.user.username), message=message)
+            if request.user != User.objects.get(username=problem.user.username):
+                Notification.objects.create(user=User.objects.get(username=problem.user.username), message=message)
+            else:
+                Notification.objects.create(user=User.objects.get(username=problem.user.username), read=True)
+
             messages.success(request, 'Görev başarıyla eklendi.')
             return redirect('user')
 
     
-    context = {'form':form}
+    context = {'form':form, 'unread_notification':unread_notification}
     return render(request,'base/add_problem.html', context)
 
 @login_required
 def notification(request):
     # Kullanıcının bildirimlerini al
     notifications = Notification.objects.filter(user=request.user).order_by('-date_created')
+    
     
     # Okunmamış bildirim sayısını al
     unread_notification = Notification.objects.filter(user=request.user, read=False).count()
